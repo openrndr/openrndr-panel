f0d23092a25e807eeb56fe1b82a84e53b59deb6f
edwin <edwin@lustlab.net>
2018-03-22 20:17:08 +0100

    Added saveFile to FileDialogs

--- src/main/kotlin/org/openrndr/panel/filedialogs/FileDialogs.kt
+++ src/main/kotlin/org/openrndr/panel/filedialogs/FileDialogs.kt
@@ -11,15 +11,26 @@ fun openFile(function: (File) -> Unit) {
     val defaultPath: CharSequence? = null
     val out = memAllocPointer(1)
     val r = NativeFileDialog.NFD_OpenDialog(filterList, defaultPath, out)
-
     if (r == NFD_OKAY) {
         val ptr = out.get(0)
         val str = memUTF8(ptr)
-
         val f = File(str)
-
         function(f)
     }
-
     memFree(out)
+}
+
+fun saveFile(function: (File) -> Unit) {
+    val filterList: CharSequence? = null
+    val defaultPath: CharSequence? = null
+    val out = memAllocPointer(1)
+    val r = NativeFileDialog.NFD_SaveDialog(filterList, defaultPath, out)
+    if (r == NFD_OKAY) {
+        val ptr = out.get(0)
+        val str = memUTF8(ptr)
+        val f = File(str)
+        function(f)
+    }
+    memFree(out)
+
 }
