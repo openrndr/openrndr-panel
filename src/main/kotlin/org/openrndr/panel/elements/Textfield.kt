8297f39a857dd0495619068e9679053a99ec71f3
Edwin Jakobs <edwin@rndr.studio>
2018-06-18 11:46:29 +0200

    Added Textfield

--- src/main/kotlin/org/openrndr/panel/elements/Textfield.kt
+++ src/main/kotlin/org/openrndr/panel/elements/Textfield.kt
@@ -2,31 +2,63 @@ package org.openrndr.panel.elements
 
 import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.Drawer
-import org.openrndr.panel.style.Color
-import org.openrndr.panel.style.background
-import org.openrndr.panel.style.color
+import org.openrndr.draw.LineCap
+import org.openrndr.panel.style.*
 import org.openrndr.text.Writer
+import rx.subjects.PublishSubject
 
-class Textfield : Element(ElementType("text-field")) {
+class Textfield : Element(ElementType("textfield")) {
 
     var value: String = ""
     var label: String = "label"
 
+    class ValueChangedEvent(val source: Textfield, val oldValue: String, val newValue: String)
     class Events {
-
+        val valueChanged: PublishSubject<ValueChangedEvent> = PublishSubject.create()
     }
 
     val events = Events()
 
     init {
+        keyboard.repeated.subscribe {
+            if (it.key == 259) {
+                if (!value.isEmpty())
+                    value = value.substring(0, value.length - 1)
+            }
+            draw.dirty = true
+            it.cancelPropagation()
+
+        }
         keyboard.pressed.subscribe {
-            value += it.name
+            if (it.key == 259) {
+                if (!value.isEmpty())
+                    value = value.substring(0, value.length - 1)
+            }
+            draw.dirty = true
+            it.cancelPropagation()
+        }
+
+        keyboard.character.subscribe {
+            val oldValue = value
+            value += it.character
+            events.valueChanged.onNext(ValueChangedEvent(this, oldValue, value))
+            it.cancelPropagation()
+        }
+
+        mouse.pressed.subscribe {
+            it.cancelPropagation()
+        }
+        mouse.clicked.subscribe {
             it.cancelPropagation()
         }
     }
 
     override fun draw(drawer: Drawer) {
-        drawer.fill = ((computedStyle.background as? Color.RGBa)?.color ?: ColorRGBa.PINK)
+        drawer.fill = computedStyle.effectiveBackground
+        drawer.stroke = null
+        drawer.rectangle(0.0, 0.0, layout.screenWidth, layout.screenHeight)
+
+
         (root() as? Body)?.controlManager?.fontManager?.let {
             val font = it.font(computedStyle)
 
@@ -35,14 +67,21 @@ class Textfield : Element(ElementType("text-field")) {
             val textWidth = writer.textWidth(value)
             val textHeight = font.ascenderLength
 
-            val offset = Math.round((layout.screenWidth - textWidth) / 2.0)
+            val offset = 5.0
             val yOffset = Math.round((layout.screenHeight / 2) + textHeight / 2.0 - 2.0) * 1.0
 
             drawer.fill = ((computedStyle.color as? Color.RGBa)?.color ?: ColorRGBa.WHITE)
-            drawer.text(label, 0.0 + offset, 0.0 + yOffset)
+            drawer.text("$label", 0.0 + offset, 0.0 + yOffset - textHeight * 1.5)
+
+
+            drawer.fill = ((computedStyle.color as? Color.RGBa)?.color ?: ColorRGBa.WHITE)
+            drawer.text("$value", 0.0 + offset, 0.0 + yOffset)
             drawer.stroke = ((computedStyle.color as? Color.RGBa)?.color ?: ColorRGBa.WHITE)
             drawer.strokeWeight = 1.0
+
+            drawer.stroke = computedStyle.effectiveColor?.shade(0.25)
+            drawer.lineCap = LineCap.ROUND
+            drawer.lineSegment(0.0, yOffset + 4.0, layout.screenWidth, yOffset + 4.0)
         }
-        drawer.rectangle(0.0, 0.0, layout.screenWidth, layout.screenHeight)
     }
 }
