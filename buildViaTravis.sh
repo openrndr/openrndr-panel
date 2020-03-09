749b7e0787f32ceaa9fa470804e2c3d16e22c7b5
Edwin Jakobs <edwin@rndr.studio>
2020-03-09 16:56:56 +0100

    Attempt to clean TRAVIS_TAG

--- buildViaTravis.sh
+++ buildViaTravis.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 # This script will build the project.
 
+CLEANED_TAG=`echo $TRAVIS_TAG | xargs`
+
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
   ./gradlew build
-elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
+elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$CLEANED_TAG" == "" ]; then
   echo -e 'Build Branch without Snapshot => Branch ['$TRAVIS_BRANCH']'
   ./gradlew -Prelease.travisci=true build --info
-elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
-  echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
-  case "$TRAVIS_TAG" in
+elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$CLEANED_TAG" != "" ]; then
+  echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$CLEANED_TAG']'
+  case "$CLEANED_TAG" in
   *-rc\.*)
     ./gradlew -Prelease.travisci=true -Prelease.useLastTag=true candidate publishNebulaPublicationToBintrayRepository --info
     ;;
@@ -18,6 +20,6 @@ elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
     ;;
   esac
 else
-  echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
+  echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$CLEANED_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
   ./gradlew build
 fi
